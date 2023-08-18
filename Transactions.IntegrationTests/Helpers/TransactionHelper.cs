d6e3e0e0f9f222f1de3c84102a6a7f4a18ab82e8
Matheus de Assis <assis4002@gmail.com>
2023-08-18 10:49:15 -0300

    build: comment old code

--- Transactions.IntegrationTests/Helpers/TransactionHelper.cs
+++ Transactions.IntegrationTests/Helpers/TransactionHelper.cs
@@ -1,48 +1,48 @@
-using Transactions.API.DTOs.Request;
-using Transactions.API.Entities;
+//using Transactions.API.DTOs.Request;
+//using Transactions.API.Entities;
 
-namespace Transactions.IntegrationTests.Helpers;
+//namespace Transactions.IntegrationTests.Helpers;
 
-internal static class TransactionHelper
-{
-    internal static TransactionCreateRequestDto GenerateCreateRequest()
-        => new()
-        {
-            Title = "title",
-            Type = TransactionType.Outcome,
-            Amount = 100,
-            Category = TransactionCategory.Purchases,
-            Date = new DateTime(2023, 01, 01)
-        };
+//internal static class TransactionHelper
+//{
+//    internal static TransactionCreateRequestDto GenerateCreateRequest()
+//        => new()
+//        {
+//            Title = "title",
+//            Type = TransactionType.Outcome,
+//            Amount = 100,
+//            Category = TransactionCategory.Purchases,
+//            Date = new DateTime(2023, 01, 01)
+//        };
 
-    internal static TransactionCreateRequestDto GenerateCreateRequest(TransactionType type)
-        => new()
-        {
-            Title = "title",
-            Type = type,
-            Amount = 56.97M,
-            Category = TransactionCategory.Purchases,
-            Date = new DateTime(2024, 01, 01)
-        };
+//    internal static TransactionCreateRequestDto GenerateCreateRequest(TransactionType type)
+//        => new()
+//        {
+//            Title = "title",
+//            Type = type,
+//            Amount = 56.97M,
+//            Category = TransactionCategory.Purchases,
+//            Date = new DateTime(2024, 01, 01)
+//        };
 
-    internal static TransactionCreateRequestDto GenerateCreateRequest(DateTime date)
-        => new()
-        {
-            Title = "title",
-            Type = TransactionType.Outcome,
-            Amount = 100,
-            Category = TransactionCategory.Purchases,
-            Date = date
-        };
+//    internal static TransactionCreateRequestDto GenerateCreateRequest(DateTime date)
+//        => new()
+//        {
+//            Title = "title",
+//            Type = TransactionType.Outcome,
+//            Amount = 100,
+//            Category = TransactionCategory.Purchases,
+//            Date = date
+//        };
 
-    internal static TransactionUpdateRequestDto GenerateUpdateRequest(string id)
-        => new()
-        {
-            Id = id,
-            Title = "title novo",
-            Type = TransactionType.Income,
-            Amount = 200,
-            Category = TransactionCategory.Food,
-            Date = new DateTime(2024, 01, 01)
-        };
-}
+//    internal static TransactionUpdateRequestDto GenerateUpdateRequest(string id)
+//        => new()
+//        {
+//            Id = id,
+//            Title = "title novo",
+//            Type = TransactionType.Income,
+//            Amount = 200,
+//            Category = TransactionCategory.Food,
+//            Date = new DateTime(2024, 01, 01)
+//        };
+//}
--- Transactions.IntegrationTests/TransactionsApplication.cs
+++ Transactions.IntegrationTests/TransactionsApplication.cs
@@ -1,89 +1,89 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using System.Net.Http.Json;
-using Transactions.API.Entities;
-using static Transactions.IntegrationTests.Helpers.TransactionHelper;
+//using Microsoft.AspNetCore.Mvc.Testing;
+//using Microsoft.Extensions.Hosting;
+//using Newtonsoft.Json;
+//using System.Net.Http.Json;
+//using Transactions.API.Entities;
+//using static Transactions.IntegrationTests.Helpers.TransactionHelper;
 
-namespace Transactions.IntegrationTests;
+//namespace Transactions.IntegrationTests;
 
-public class TransactionsApplication : WebApplicationFactory<Program>, IDisposable
-{
-    private readonly HttpClient _httpClient;
-    private const string _baseUrl = "https://localhost:5098/transaction";
+//public class TransactionsApplication : WebApplicationFactory<Program>, IDisposable
+//{
+//    private readonly HttpClient _httpClient;
+//    private const string _baseUrl = "https://localhost:5098/transaction";
 
-    public TransactionsApplication()
-    {
-        _httpClient = CreateClient(_baseUrl);
-        CreateBaseTest().GetAwaiter();
-    }
+//    public TransactionsApplication()
+//    {
+//        _httpClient = CreateClient(_baseUrl);
+//        CreateBaseTest().GetAwaiter();
+//    }
 
-    public new void Dispose() => RemoveBaseTest().GetAwaiter();
+//    public new void Dispose() => RemoveBaseTest().GetAwaiter();
 
-    protected override IHost CreateHost(IHostBuilder builder)
-    {
-        builder.UseEnvironment("IntegrationTests");
-        return base.CreateHost(builder);
-    }
+//    protected override IHost CreateHost(IHostBuilder builder)
+//    {
+//        builder.UseEnvironment("IntegrationTests");
+//        return base.CreateHost(builder);
+//    }
 
-    private async Task CreateBaseTest()
-    {
-        var income = GenerateCreateRequest(TransactionType.Income);
-        var outcome = GenerateCreateRequest(TransactionType.Outcome);
+//    private async Task CreateBaseTest()
+//    {
+//        var income = GenerateCreateRequest(TransactionType.Income);
+//        var outcome = GenerateCreateRequest(TransactionType.Outcome);
 
-        await Post(income);
-        await Post(income);
-        await Post(outcome);
-        await Post(outcome);
-    }
+//        await Post(income);
+//        await Post(income);
+//        await Post(outcome);
+//        await Post(outcome);
+//    }
 
-    private async Task RemoveBaseTest() => await DeleteByPeriod<string>("012024");
+//    private async Task RemoveBaseTest() => await DeleteByPeriod<string>("012024");
 
-    private HttpClient CreateClient(string url)
-    {
-        var httpClientOptions = new WebApplicationFactoryClientOptions
-        {
-            BaseAddress = new Uri(url)
-        };
+//    private HttpClient CreateClient(string url)
+//    {
+//        var httpClientOptions = new WebApplicationFactoryClientOptions
+//        {
+//            BaseAddress = new Uri(url)
+//        };
 
-        return CreateClient(httpClientOptions);
-    }
+//        return CreateClient(httpClientOptions);
+//    }
 
-    public async Task<TResponse> Get<TResponse>(string? period)
-    {
-        var result = await _httpClient.GetAsync($"{_httpClient.BaseAddress}/{period}");
-        var responseString = await result.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<TResponse>(responseString);
-    }
+//    public async Task<TResponse> Get<TResponse>(string? period)
+//    {
+//        var result = await _httpClient.GetAsync($"{_httpClient.BaseAddress}/{period}");
+//        var responseString = await result.Content.ReadAsStringAsync();
+//        return JsonConvert.DeserializeObject<TResponse>(responseString);
+//    }
 
-    public async Task Post<TRequest>(TRequest request)
-        => await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, request);
+//    public async Task Post<TRequest>(TRequest request)
+//        => await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, request);
 
-    public async Task<TResponse> Post<TRequest, TResponse>(TRequest request)
-    {
-        var result = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, request);
-        var responseString = await result.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<TResponse>(responseString);
-    }
+//    public async Task<TResponse> Post<TRequest, TResponse>(TRequest request)
+//    {
+//        var result = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, request);
+//        var responseString = await result.Content.ReadAsStringAsync();
+//        return JsonConvert.DeserializeObject<TResponse>(responseString);
+//    }
 
-    public async Task<TResponse> Put<TRequest, TResponse>(TRequest request)
-    {
-        var result = await _httpClient.PutAsJsonAsync(_httpClient.BaseAddress, request);
-        var responseString = await result.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<TResponse>(responseString);
-    }
+//    public async Task<TResponse> Put<TRequest, TResponse>(TRequest request)
+//    {
+//        var result = await _httpClient.PutAsJsonAsync(_httpClient.BaseAddress, request);
+//        var responseString = await result.Content.ReadAsStringAsync();
+//        return JsonConvert.DeserializeObject<TResponse>(responseString);
+//    }
 
-    public async Task<TResponse> Delete<TResponse>(string? id)
-    {
-        var result = await _httpClient.DeleteAsync($"{_httpClient.BaseAddress}/{id}");
-        var responseString = await result.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<TResponse>(responseString);
-    }
+//    public async Task<TResponse> Delete<TResponse>(string? id)
+//    {
+//        var result = await _httpClient.DeleteAsync($"{_httpClient.BaseAddress}/{id}");
+//        var responseString = await result.Content.ReadAsStringAsync();
+//        return JsonConvert.DeserializeObject<TResponse>(responseString);
+//    }
 
-    public async Task<TResponse> DeleteByPeriod<TResponse>(string? period)
-    {
-        var result = await _httpClient.DeleteAsync($"{_httpClient.BaseAddress}/byperiod/{period}");
-        var responseString = await result.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<TResponse>(responseString);
-    }
-}
+//    public async Task<TResponse> DeleteByPeriod<TResponse>(string? period)
+//    {
+//        var result = await _httpClient.DeleteAsync($"{_httpClient.BaseAddress}/byperiod/{period}");
+//        var responseString = await result.Content.ReadAsStringAsync();
+//        return JsonConvert.DeserializeObject<TResponse>(responseString);
+//    }
+//}
